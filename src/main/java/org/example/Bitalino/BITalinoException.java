b0d71abac3f1c7f49ccf1b8ba97ee6dabf9cbf57
PaulaMiron <paula.mirongracia@usp.ceu.es>
2025-11-03 21:04:56 +0100

    Bitalino done

---
+++ src/main/java/org/example/Bitalino/BITalinoException.java
@@ -0,0 +1,12 @@
+package org.example.Bitalino;
+
+public class BITalinoException extends Exception{
+    private static final long serialVersionUID = 1L;
+
+    public BITalinoException(BITalinoErrorTypes errorType) {
+        super(errorType.getName());
+        code = errorType.getValue();
+    }
+
+    public int code;
+}
