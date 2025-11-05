9f841e27700a5a1094e168b424641291b7e118cd
martagonzalezz <marta.gonzalezromero@usp.ceu.es>
2025-11-05 15:27:49 +0100

    JDBCRecordingManager, RecordingManager modified

--- src/main/java/org/example/IFaces/RecordingManager.java
+++ src/main/java/org/example/IFaces/RecordingManager.java
@@ -5,4 +5,5 @@ import org.example.POJOS.Recording;
 public interface RecordingManager {
     public void addRecording(Recording recording);
     public void deleteRecording (Integer recording_id);
+    //public Recording getRecordingById(Integer recording_id);
 }
--- src/main/java/org/example/JDBC/JDBCRecordingManager.java
+++ src/main/java/org/example/JDBC/JDBCRecordingManager.java
@@ -4,10 +4,9 @@ import org.example.IFaces.RecordingManager;
 import org.example.POJOS.Patient;
 import org.example.POJOS.Recording;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.lang.reflect.Type;
+import java.sql.*;
+import java.time.LocalDate;
 
 public class JDBCRecordingManager implements RecordingManager {
     private JDBCManager manager;
@@ -17,7 +16,7 @@ public class JDBCRecordingManager implements RecordingManager {
     }
 
     public void addRecording(Recording recording) {
-        String sql = "INSERT INTO Recordings (type, recordingDate) VALUES (?, ?, ?)";
+        String sql = "INSERT INTO Recordings (type, recordingDate, patient_id) VALUES (?, ?, ?)";
 
         try{
             PreparedStatement ps = manager.getConnection().prepareStatement(sql);
@@ -25,6 +24,7 @@ public class JDBCRecordingManager implements RecordingManager {
 
             ps.setString(1, recording.getType());
             ps.setDate(2, recording.getDateRecording());
+            //ps.setInt(3, recording.getId())
             //Ns si tmb hay que meter el patient_id
 
             ps.executeUpdate();
@@ -45,21 +45,31 @@ public class JDBCRecordingManager implements RecordingManager {
             e.printStackTrace();
         }
     }
-
-  /*  public Recording getRecordingById(Integer id){
+/*
+    public Recording getRecordingById(Integer recording_id) {
         Recording recording = null;
-        try{
-            Statement stmt = manager.getConnection().createStatement();
-            String sql = "SELECT * FROM recording WHERE id = " + id;
-            ResultSet rs = stmt.executeQuery(sql);
 
-        }
-        catch(Exception e){
+        try {
+            Statement stmt = manager.getConnection().createStatement();
+            String sql = "SELECT * FROM Recordings WHERE recording_id = " + recording_id;
+
+            ResultSet rs = stmt.executeQuery(sql);
+            if (rs.next()) {
+                LocalDate dateRecording = rs.getDate("recordingDate").toLocalDate();
+                Type type = rs.getString("type");
+                //Integer patient_id = rs.getInt("patient_id");
+                recording = new Recording(recording_id, dateRecording, type);
+
+            }
+
+            rs.close();
+            stmt.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return recording;
     }
 
-    */
+*/
 
 }
