3503b2d06bb6624cf6bee1ff13d1156f8ca51f87
NereaLeria <nerea.leriapareja@usp.ceu.es>
2025-11-14 18:55:53 +0100

    Connection With Server Update

--- Patient_App/src/main/java/Connection/Connection_With_Server.java
+++ Patient_App/src/main/java/Connection/Connection_With_Server.java
@@ -1,5 +1,16 @@
 package Connection;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Connection_With_Server {
+    public static void main(String ip_host) throws IOException {
+        Socket socket = new Socket(ip_host, 9000);
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
 
 }
