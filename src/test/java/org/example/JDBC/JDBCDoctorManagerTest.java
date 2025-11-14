81669b512198d169244d5d7c544d0a552401b8dc
maria20040717 <maria.martinezvillarrasa@usp.ceu.es>
2025-11-14 16:16:43 +0100

    created tests section

---
+++ src/test/java/org/example/JDBC/JDBCDoctorManagerTest.java
@@ -0,0 +1,19 @@
+package org.example.JDBC;
+
+import org.example.POJOS.Doctor;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JDBCDoctorManagerTest {
+
+    @Test
+    void TestaddDoctor() throws Exception{
+
+
+
+    }
+}
