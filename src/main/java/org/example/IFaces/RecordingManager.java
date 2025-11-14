75bfedc179fdce7a0ede91e9417cf179323f5fb6
martagonzalezz <marta.gonzalezromero@usp.ceu.es>
2025-11-14 16:58:04 +0100

    POJOS

--- src/main/java/org/example/IFaces/RecordingManager.java
+++
@@ -1,9 +0,0 @@
-package org.example.IFaces;
-
-import org.example.POJOS.Recording;
-
-public interface RecordingManager {
-    public void addRecording(Recording recording);
-    public void deleteRecording (Integer recording_id);
-    //public Recording getRecordingById(Integer recording_id);
-}
