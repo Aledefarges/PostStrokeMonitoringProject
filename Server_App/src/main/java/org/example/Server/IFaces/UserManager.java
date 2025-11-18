e5c1c1f0a5cfda526e55f9908143d999b072c7af
martagonzalezz <marta.gonzalezromero@usp.ceu.es>
2025-11-18 10:50:02 +0100

    USER

--- Server_App/src/main/java/org/example/Server/IFaces/UserManager.java
+++ Server_App/src/main/java/org/example/Server/IFaces/UserManager.java
@@ -3,8 +3,8 @@ package org.example.Server.IFaces;
 import org.example.Server.POJOS.User;
 
 public interface UserManager {
-    User login(String username, String password);
-    boolean logout(int user_id);
-    boolean register(User user);
-    boolean changePassword(int user_id, String new_passwd);
+    public User login(String username, String password);
+    public boolean logout(int user_id);
+    public boolean register(User user);
+    public void changePassword(User user, String new_passwd);
 }
