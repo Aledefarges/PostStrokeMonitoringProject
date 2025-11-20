44a7a480940c20b1b2a42dd16dec0d94200717c9
maria20040717 <maria.martinezvillarrasa@usp.ceu.es>
2025-11-20 13:27:56 +0100

    corrected the class JDBCUserManager methods

--- Server_App/src/main/java/org/example/Server/IFaces/UserManager.java
+++ Server_App/src/main/java/org/example/Server/IFaces/UserManager.java
@@ -6,7 +6,7 @@ public interface UserManager {
     public void addUser(User user);
     public void deleteUserByEmail(String email);
     public void deleteUserById(int user_id);
-    public void changePassword(User user, String new_password);
+    public void changePassword(int user_id, String new_password);
     public boolean checkPassword(String username, String password);
     public void changeEmail(int user_id, String email);
     public User getUserByEmail(String email);
