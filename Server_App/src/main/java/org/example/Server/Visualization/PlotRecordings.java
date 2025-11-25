abb4a1f8079379540b1260342a9fba93caef8cf1
PaulaMiron <paula.mirongracia@usp.ceu.es>
2025-11-25 20:09:59 +0100

    recording visualization changes

--- Server_App/src/main/java/org/example/Server/Visualization/PlotRecordings.java
+++ Server_App/src/main/java/org/example/Server/Visualization/PlotRecordings.java
@@ -73,6 +73,9 @@ public static void showChartFromArray(Double[] data, String title) {
     double min = Arrays.stream(data).mapToDouble(v -> v).min().orElse(0);
     double max = Arrays.stream(data).mapToDouble(v -> v).max().orElse(1);
 
+    if (min == max) {
+        max = min + 1;
+    }
     // Expandir el rango para ver la señal
     double margin = (max - min) * 0.1;
     plot.getRangeAxis().setRange(min-margin, max+margin);
