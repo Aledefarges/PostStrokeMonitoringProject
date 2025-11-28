673998ae9b80be3e52288109d33e75e7cbc9545d
NereaLeria <nerea.leriapareja@usp.ceu.es>
2025-11-28 13:03:11 +0100

    Changes JDBC DOCTOR

---
+++ Server_App/src/main/java/org/example/Server/GUI/AdminPanel.java
@@ -0,0 +1,52 @@
+/*
+ * Created by JFormDesigner on Fri Nov 28 13:01:45 CET 2025
+ */
+
+package org.example.Server.GUI;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @author nerea
+ */
+public class AdminPanel extends JPanel {
+    public AdminPanel() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        // Generated using JFormDesigner Evaluation license - Nerea Leria
+        label = new JLabel();
+        password_field = new JTextField();
+
+        //======== this ========
+        setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax . swing. border .EmptyBorder (
+        0, 0 ,0 , 0) ,  "JFor\u006dDesi\u0067ner \u0045valu\u0061tion" , javax. swing .border . TitledBorder. CENTER ,javax . swing. border .TitledBorder
+        . BOTTOM, new java. awt .Font ( "Dia\u006cog", java .awt . Font. BOLD ,12 ) ,java . awt. Color .
+        red ) , getBorder () ) );  addPropertyChangeListener( new java. beans .PropertyChangeListener ( ){ @Override public void propertyChange (java .
+        beans. PropertyChangeEvent e) { if( "bord\u0065r" .equals ( e. getPropertyName () ) )throw new RuntimeException( ) ;} } );
+        setLayout(new GridBagLayout());
+        ((GridBagLayout)getLayout()).columnWidths = new int[] {356, 0};
+        ((GridBagLayout)getLayout()).rowHeights = new int[] {0, 0, 0};
+        ((GridBagLayout)getLayout()).columnWeights = new double[] {0.0, 1.0E-4};
+        ((GridBagLayout)getLayout()).rowWeights = new double[] {0.0, 0.0, 1.0E-4};
+
+        //---- label ----
+        label.setText("Enter password to close SERVER");
+        add(label, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 5, 0), 0, 0));
+        add(password_field, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 0, 0), 0, 0));
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Evaluation license - Nerea Leria
+    private JLabel label;
+    private JTextField password_field;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
