6e61799df3022267530a7779455104456e2b99f4
NereaLeria <nerea.leriapareja@usp.ceu.es>
2025-11-28 09:32:17 +0100

    Interfaz grafica dcotor

--- Doctor_App/src/main/java/GUI/AppFrameDoctor.java
+++ Doctor_App/src/main/java/GUI/AppFrameDoctor.java
@@ -8,7 +8,7 @@ public class AppFrameDoctor extends JFrame{
     private Connection_Doctor connection;
     public AppFrameDoctor() {
         connection = new Connection_Doctor();
-        boolean ok = connection.connection("localhost",9000);
+        boolean ok = connection.connection("10.60.102.67",9000);
 
         setTitle("Patient App");
         setSize(600,500);
